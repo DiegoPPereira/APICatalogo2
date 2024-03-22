4cae1adde63e37f9008d0c1163f889863fb0244f
DiegoPPereira <diegottp50@hotmail.com>
2024-03-22 19:43:08 -0300

    Se este commit for implementado ele irá conseguir fazer tratamento de erros nos endpoints com try catch

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,88 @@
+﻿using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+
+
+        private readonly AppDbContext? _context;git
+
+        public ProdutosController(AppDbContext? context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var Produtos = _context.Produtos.Take(10).ToList();
+            if (Produtos is null) {
+
+                return NotFound("Produtos não encontrados...");
+
+            }
+            return Produtos;
+
+        }
+        [HttpGet("{id:int}", Name = "ObterProdutos")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var Produtos = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (Produtos is null)
+            {
+
+                return NotFound("1 Produtos não encontrados...");
+
+            } return Produtos;
+        }
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto == null)
+                return BadRequest("1 Produtos não criado...");
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProdutos",
+                new { id = produto.ProdutoId }, produto);
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+
+            if (id != produto.ProdutoId)
+                return BadRequest();
+            
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+           var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+        } 
+       
+
+    }
+}
---
+++ Migrations/PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Produtos (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId )" + 
+                "VALUES('Coca-Cola Diet','Sem proteínas, gorduras ou fibra significativas. Valores diários baseados em uma dieta de 2000 kcal ou 8400 kj.',6.50,'cocacola.jpg',50,now(),1)");
+
+            migrationBuilder.Sql("INSERT INTO Produtos (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId )" +
+                "VALUES('Torta de Atum','Torta de Atum 1000kg.',11.55,'torta.jpg',150,now(),2)");
+
+            migrationBuilder.Sql("INSERT INTO Produtos (Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId )" +
+                "VALUES('Bolo','bolo de cenoura 250kg.',10.75,'bolo.jpg',550,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
